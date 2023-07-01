8b6a23ad955d05d201987b673497113402831221
Glebson Santos <glebsonsantos@outlook.pt>
2023-07-01 19:04:19 -0300

    Adicionando o arquivo de Contexto, e a 1 migration.

---
+++ ProEventos/Src/BackEnd/ProEventos.Indraestructure/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Infraestructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+
+    }
+}
---
+++ ProEventos/Src/BackEnd/ProEventos.Indraestructure/Migrations/20230701205940_Criando Entidade Evento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProEventos.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoEntidadeEvento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Local = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataEvento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tema = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    Lote = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Eventos");
+        }
+    }
+}
