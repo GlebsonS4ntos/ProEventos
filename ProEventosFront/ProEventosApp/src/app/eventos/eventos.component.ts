947eab6a109ec9c2824e69c6723d183e4dd4bf39
Glebson Santos <glebsonsantos@outlook.pt>
2023-12-28 23:43:00 -0300

    Criacao da NavBar e implementando uma tabela pra exibicao dos eventos

--- ProEventosFront/ProEventosApp/src/app/eventos/eventos.component.ts
+++ ProEventosFront/ProEventosApp/src/app/eventos/eventos.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+  showImagens : boolean = true;
+  widthImagemEvento : number = 100;
+  eventos : any = [];
 
-  eventos : any;
+  filtroEvento : string = "";
 
   constructor(private http : HttpClient) { }
 
@@ -19,8 +22,21 @@ export class EventosComponent implements OnInit {
   public GetEventos() : void {
     this.http.get('https://localhost:44339/api/Evento').subscribe(
       response => this.eventos = response,
-      error => console.log(error)
+      error =>  console.log(error)
     );
+  }
+
+  public MudarStatusDeExibicaoDasImagens(): void {
+    this.showImagens = !this.showImagens;
+    let botaoImagem = document.getElementById("botaoMudarVisualizacaoImagem") as HTMLButtonElement;
+    if(this.showImagens == false){
+      botaoImagem.classList.remove("btn-primary");
+      botaoImagem.classList.add("btn-danger");
+    }else{
+      botaoImagem.classList.remove("btn-danger");
+      botaoImagem.classList.add("btn-primary");
+    }
+
   }
 
 }
